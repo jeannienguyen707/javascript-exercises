b23198a506f6357529c34c63d279b2c2f1e34437
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-07-27 19:34:18 -0700

    awoifhwoefj

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
 const sumAll = function() {
-
-};
+    if (arguments[0] <0 || arguments[1] <0) {
+        return 'ERROR'
+    }
+    
+    let sum = 0
+    for (let i = arguments[0]; i <= arguments[1] ; i++){
+        console.log(sum)
+        sum+=i
+    }
+    
+    return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
