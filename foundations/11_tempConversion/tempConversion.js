c8ebf86fd75e3cee1ffa0a3612876f34af19c64c
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-07-28 12:40:30 -0700

    Use formula online and round with math.round
    Mimic conversion formulas online, for 1 digit
    decimal rounding involves rounding after moving
    the digit to the right once. Round this result
    and than shift the digit back left.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
 const convertToCelsius = function() {
+  let num = arguments[0]
+  num = ((num -32)*5)/9
+  console.log(Math.round(num*10)/10)
+  num = Math.round(num*10)/10
+  return num
 };
 
 const convertToFahrenheit = function() {
+  let num = arguments[0]
+  num = (num * (9/5)) +32
+  num = Math.round(num*10)/10
+  return num
 };
 
 // Do not edit below this line
