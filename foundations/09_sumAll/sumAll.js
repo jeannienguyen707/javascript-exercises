e6cda3afaf9ec3381ce9c1b24a6e1771769c90a5
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-07-28 12:12:48 -0700

    Use .isInteger to verify arguments
    Verify arguments are integer values, determine
    which argument is the larger value and use a
    loop to add starting from the smallest value to
    and including the largest value, a return the
    total sum

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,10 +2,18 @@ const sumAll = function() {
     if (arguments[0] <0 || arguments[1] <0) {
         return 'ERROR'
     }
-    
+      if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1])) {
+        return 'ERROR'
+    }
+
+    let left = arguments[0]
+    let right =  arguments[1]
+    if (left > right) {
+        left = arguments[1]
+        right = arguments[0]
+    }
     let sum = 0
-    for (let i = arguments[0]; i <= arguments[1] ; i++){
-        console.log(sum)
+    for (let i = left; i <= right ; i++){
         sum+=i
     }
     
