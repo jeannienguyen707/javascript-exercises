b62387fa5784bf195e012d96f184a5eefdd8045d
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-07-27 18:31:04 -0700

    Loop Argument Object and compare with strict equal
    Strict equal ensures removal of elements to be
    exact while Argument allows range of arguments

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,10 +1,19 @@
-function callME(num){
-    return (arr.filter(item => item != num))
-}
-const removeFromArray = function(arr, val) {
-    
-    return val.forEach(num => callME(num))
+
+const removeFromArray = function() {
+
+    // for (const arg of arguments)
+    //     result.filter(item => item != val[i])
+        
+    // }
+    // return result
+    for (let i = 1; i < arguments.length; i++) {
+        
+        arguments[0] = arguments[0].filter(items => (items !== arguments[i]))
+        console.log(arguments[0])
+    }
+    return arguments[0]
 };
+
 
 
 // Do not edit below this line
