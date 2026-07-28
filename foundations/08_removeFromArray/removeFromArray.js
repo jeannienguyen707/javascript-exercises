bce9c16dafaa345598f2f1efc7445afa83837124
Jeannie Nguyen <Jeannienguyen888@gmail.com>
2026-07-27 17:52:28 -0700

    Forgot what I changed

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+function callME(num){
+    return (arr.filter(item => item != num))
+}
+const removeFromArray = function(arr, val) {
+    
+    return val.forEach(num => callME(num))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
